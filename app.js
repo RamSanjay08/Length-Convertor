3f0d2251af52d30ae372933b87cf1ec9741757ac
Ram Sanjay <ramsanjaydev08@gmail.com>
2023-09-22 16:35:53 +0530

    updated

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-function convert() {
-const cmVal = Number(document.getElementById("input").value)
-const inchVal = cmVal/2.34
-const result = document.getElementById("result")
-result.innerHTML = inchVal.toFixed(3) + " inches"
-}
+let inchbtn = document.querySelector(".inch-btn")
+let abc = document.querySelector(".main-container h1");
+let meterbtn = document.querySelector(".meter-btn");
 
+
+inchbtn.addEventListener('click' ,
+function () {
+  const cmVal = Number(document.getElementById("input").value)
+  const inchVal = cmVal/2.54
+ abc.textContent=inchVal.toFixed(3)
+  }
+)
+
+meterbtn.addEventListener('click' ,
+function () {
+  const cmVal = Number(document.getElementById("input").value)
+  const meterVal = cmVal*0.01
+  abc.textContent = meterVal
+  }
+)
+// console.log(document.querySelector(".main-container h1").textContent);
